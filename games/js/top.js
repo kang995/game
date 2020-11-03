e58ac6ac6bd1a6171c3c90730610ad428cfe78f5
每天都要努力 <52522370+caojishuai520520@users.noreply.github.com>
2020-11-03 21:43:39 +0800

    11

--- games/js/top.js
+++ games/js/top.js
@@ -1,3 +1,16 @@
-var img = document.getElementById('overlay')
+var obj = {
+    id:'overlay',
+    id1:'imgs',
+    tyoe:function(){
+        var dom = document.getElementById(this.id)
+        var img = document.getElementById(this.id1)
+        this.typeOf(dom,img)
+    },
+    typeOf:function(dom,img){
+      img.onclick = function(){
+          dom.style.display = 'none'
+      }
+    }
+}
+obj.tyoe();
 
-console.log(img)
