0160a727ebeae58aa8438c078d1aded6bb276be1
每天都要努力 <52522370+caojishuai520520@users.noreply.github.com>
2020-11-04 17:47:11 +0800

    修改 用户模糊名称

--- games/js/top.js
+++ games/js/top.js
@@ -4,10 +4,15 @@ var obj = {
     tyoe:function(){
         var img = document.getElementById(this.id1)
         img.onclick = this.typeOf
+        this.slice()
     },
     typeOf:function(){
         document.getElementById('overlay').style.display = 'none'
-        // window.Sidebar.hide()
+    },
+    slice:function(){
+        var name = document.getElementById('name');
+        var str = name.innerHTML.slice(0,1) +'*'.repeat(7)+ name.innerHTML.slice(8)
+        name.innerHTML = str
     }
 }
 obj.tyoe();
